e24e2d6f88338422c142526dfd61aa04ffa140eb
Pheet0123 <pheet0123@gmail.com>
2024-11-04 22:22:27 +0700

    เปลื่ยนชื่อ

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ function clearDisplay(){
     display.value = "";
 }
 
+function deleteLastCharacter() {
+    const display = document.getElementById("display"); 
+    display.value = display.value.slice(0, -1); }
+
 function calculate(){
     try{
 
